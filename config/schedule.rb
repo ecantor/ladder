078c8949cc6d31ec4581eb77750e3dbd2427f6f5
Eric Cantor <eric@mobileglobal.com>
2013-03-26 19:09:30 -0400

    undid validation on chalelnges

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
 set :output, "#{path}/log/cron.log"
 
-every :monday, at: "00:01" do
-  runner "RatingPeriodProcessor.perform"
-end
+# every :monday, at: "00:01" do
+#  runner "RatingPeriodProcessor.perform"
+# end
 
 every :day, :at => "00:00" do
   runner "ChallengeProcessor.perform"
